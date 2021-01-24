12b3546a88be543fe01305f02ff091323c5e5073
sloanlacey <sloanlacey89@gmail.com>
2021-01-24 15:42:45 -0800

    Add files to folder structure, lay out boiler plate code for the js files with classes and extended classes, add download dependencies.

---
+++ index.js
@@ -0,0 +1,9 @@
+const Employee = require('./lib/Employee');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+
+const inquirer = require('inquirer');
+const fs = require('fs');
+const path = require('path');
+
