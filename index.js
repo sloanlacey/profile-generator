4095b93d4ff9308a1f376da93150e762053336c3
sloanlacey <sloanlacey89@gmail.com>
2021-01-26 16:17:21 -0800

    Add boiler plate html, create function to start generating html from boiler plate code, include error handling function inside generateHtml.

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ const fs = require('fs');
 const employees = [];
 
 function initGenerator() {
+    generateHtml();
     addEmployee();
-
 }
 
 function addEmployee() {
@@ -75,10 +75,43 @@ function addEmployee() {
             } else {
                 // call function to stop rendering html?
             }
-            });
+        });
     });
-    });
+});
 }
+// Function generates the boiler plate html to start the new page
+// Has an error handling function inside generateHtml that prints 'error' to the console log
+function generateHtml() {
+    const html = `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp" crossorigin="anonymous">
+        <link rel="stylesheet" href="./index.css">
+        <title>Employee Generator</title>
+    </head>
+    <body>
+    
+    <div class="header">
+        <div class="row">
+            <div class="col s12 m12 l12 jumbotron">Meet the Team</div>
+        </div>
+    </div>
+    
+    <div class="container">
+    
+    <div class="row">`;
+fs.writeFile('./dist/new-team.html', html, function (error) {
+    if (error) {
+        console.log(error);
+    }
+})
+console.log('start');
+}
+
+
 // .then make a function to handle the name, id, email and role selection
 // start with if statements and then make switch cases
 // if role === blank then do this, else if blank do this, else do blank and pass through the unique selectors for the employee types
